06d3fc0766970a6384253ca4a5bbfe9c2b62e929
mfwolffe <mfwolffe@outlook.com>
2026-07-25 23:40:57 -0400

    facsimile 0.21.0

--- Formula/facsimile.rb
+++ Formula/facsimile.rb
@@ -1,8 +1,8 @@
 class Facsimile < Formula
   desc "Terminal text editor written in Fortran with VSCode-style keybindings"
   homepage "https://github.com/FortranGoingOnForty/facsimile"
-  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.20.1.tar.gz"
-  sha256 "3fd40c71968ffc3d57d72a46adc3be822ba30518ebcd3b625aba27b9b80e840e"
+  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.21.0.tar.gz"
+  sha256 "acce3cdd7decce82ad39a87a82489fdba02c686d6a8d16ad1b50b5e09e813b24"
   license "MIT"
   head "https://github.com/FortranGoingOnForty/facsimile.git", branch: "trunk"
 
